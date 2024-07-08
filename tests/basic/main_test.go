1dc55fa2eafb085bbc0c4adbadc2e895e78eddba
Khoi Ng <khoinguyenae@gmail.com>
2024-07-08 08:06:41 +0700

    unit-test'

---
+++ tests/basic/main_test.go
@@ -0,0 +1,46 @@
+package basic
+
+import (
+	"testing"
+)
+
+func TestAddOne(t *testing.T) {
+	var (
+		input  = 1
+		output = 3
+	)
+
+	actual := AddOne(1)
+	if actual != output {
+		t.Errorf("AddOne(%d), input %d, actual = %d", input, output, actual)
+	}
+
+	// assert.Equal(t, AddOne(2), 3, "AddOne(2) should be 3")
+	// assert.NotEqual(t, 2, 3)
+	// assert.Nil(t, nil, nil)
+}
+func TestAddOne2(t *testing.T) {
+	var (
+		input  = 1
+		output = 3
+	)
+
+	actual := AddOne2(1)
+	if actual != output {
+		t.Errorf("AddOne(%d), input %d, actual = %d", input, output, actual)
+	}
+
+	// assert.Equal(t, AddOne(2), 3, "AddOne(2) should be 3")
+	// assert.NotEqual(t, 2, 3)
+	// assert.Nil(t, nil, nil)
+}
+
+// func TestRequire(t *testing.T) {
+// 	require.Equal(t, 2, 3)
+// 	fmt.Println("Not executing")
+// }
+
+// func TestAssert(t *testing.T) {
+// 	assert.Equal(t, 2, 3)
+// 	fmt.Println("Executing")
+// }
